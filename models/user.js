d0e3bddca2fc993072f640d4c682417fa9659cfe
Mona Lisa <stefanibusiness@gmail.com>
2022-08-15 18:29:01 -0700

    created user model and connection.js

---
+++ models/user.js
@@ -0,0 +1,38 @@
+// import mongoose from "mongoose";
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            require: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            require: true,
+            unique:  true,
+            match: [/.+@.+\..+/,'Please provide valid e-mail address!!']
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+userSchema.virtual('friendCount').get(() => this.friends.length);
+
+const User = model('User', userSchema);
+
+module.exports = User;
